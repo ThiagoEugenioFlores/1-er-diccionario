7cf79e1319d60814c7422ba2c980044c97bd5531
ThiagoEugenioFlores <omer103135@gmail.com>
2025-07-21 19:39:38 -0500

    Create diccionario de memes.py

---
+++ diccionario de memes.py
@@ -0,0 +1,15 @@
+print("Bienvenido al diccionario moderno")
+print("Escribe palabras que no entiendas y con mayuscula")
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "AFK": "Estar ausente ",
+            "CREEPY":"Aterrador,siniestro",
+            "SHEESH" - "ligera desaprobación"
+            }
+for i in range(5):            
+         
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+  if word in meme_dict.keys():
+    print(word + " : " + meme_dict[word
+            
