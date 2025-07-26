91f558f2adef244d49387b12fcbec127448a9c4b
ThiagoEugenioFlores <omer103135@gmail.com>
2025-07-25 19:02:57 -0500

    Update diccionario de memes.py

--- diccionario de memes.py
+++ diccionario de memes.py
@@ -5,11 +5,12 @@ meme_dict = {
             "LOL": "Una respuesta común a algo gracioso",
             "AFK": "Estar ausente ",
             "CREEPY":"Aterrador,siniestro",
-            "SHEESH" - "ligera desaprobación"
+            "SHEESH" : "ligera desaprobación"
             }
 for i in range(5):            
-         
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
-  if word in meme_dict.keys():
-    print(word + " : " + meme_dict[word
+    word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+    if word in meme_dict.keys():
+        print(word + " : " + meme_dict[word] )
+    else:
+        print("esa palabra no esta en el diccionario intenta con otra y hazlo con mayuscula")
             
